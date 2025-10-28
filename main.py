631d0de631e8cbc19f32402f896c1ca3b57c3dff
BarryLabs <wave.carton247@aleeas.com>
2025-10-28 12:49:47 -0400

    testing

--- main.py
+++ main.py
@@ -1,89 +1,64 @@
 import json
-import os
 import sys
 
-import customtkinter
+import customtkinter as ctk
+import pandas
 import requests
 
 url = "https://www.cheapshark.com/api/1.0/"
 top15deals = "https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15"
 
-
-# class App(customtkinter.CTk):
-#     def __init__(self):
-#         self.NAME = "Papyrus"
-#         self.HEIGHT = 300
-#         self.WIDTH = 500
-#         self.STYLE = "Dark"
-#         self.COLOR = "green"
-
-#         self.customtkinter.set_appearance_mode(self.STYLE)
-#         self.customtkinter.set_default_color_theme(self.COLOR)
-
-#         self.app = customtkinter.CTk()
-#         self.app.title(self.NAME)
-#         self.app.geometry(str(self.WIDTH) + "x" + str(self.HEIGHT))
-
-#         self.button = customtkinter.CTkButton(
-#             App, text="Get Deals", command=topFifteen()
-#         )
-#         self.button.pack(pady=80)
-
-#         self.app.mainloop()
-#         return App.mainloop()
+STYLE = "Dark"
+COLOR = "green"
+ctk.set_appearance_mode(STYLE)
+ctk.set_default_color_theme(COLOR)
 
 
-def getRequest(url):
-    r = requests.get(url)
-    if r.status_code == 200:
-        data = r.json()
-        return data
-    else:
-        print("Website is Down. <Non-200>")
-        sys.exit()
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.NAME = "Bazaar"
+        self.HEIGHT = 300
+        self.WIDTH = 500
 
+        self.data = self.load_data("data.json")
 
-def postRequest(url, params):
-    params = []
+        self.frame = ctk.CTkFrame(self, corner_radius=10)
+        self.frame.pack(pady=4, padx=4, fill="both", expand=True)
 
-    # Handle Parameter Manipualtion
+        self.title_label = ctk.CTkLabel(self.frame, text=self.NAME)
+        self.title_label.pack(fill="x")
 
-    r = requests.post(url, params)
-    if r.status_code == 200:
-        data = r.json()
-        return data
-    else:
-        print("Website is Down. <Non-200>")
-        sys.exit()
+        self.title_entry = ctk.CTkEntry(self.frame)
+        self.title_entry.insert(0, self.data[0]["title"])
+        self.title_entry.pack()
 
+        self.sale_price_label = ctk.CTkLabel(self.frame, text="Sale Price")
+        self.sale_price_label.pack(fill="x")
 
-def writeData(data):
-    try:
-        with open("data.json", "w") as f:
-            json.dump(data, f, ensure_ascii=False, indent=2)
-            f.close()
-    except FileNotFoundError:
-        print("File Not Found.")
-        sys.exit()
+        self.sale_price_entry = ctk.CTkEntry(self.frame)
+        self.sale_price_entry.insert(0, self.data[0]["salePrice"])
+        self.sale_price_entry.pack()
 
+        self.store_id_label = ctk.CTkLabel(self.frame, text="Store ID")
+        self.store_id_label.pack(fill="x")
 
-def cleanData(file):
-    try:
-        with open("data.json", "r") as f:
-            data = json.load(f)
-            f.seek(0)
+        self.store_id_entry = ctk.CTkEntry(self.frame)
+        self.store_id_entry.insert(0, self.data[0]["storeID"])
+        self.store_id_entry.pack()
 
-            titles = dict(data["title"])
-            storeIDs = dict(data["storeID"])
-
-            print(titles)
-
-    except FileNotFoundError:
-        sys.exit()
+    def load_data(self, file):
+        try:
+            with open(file) as f:
+                return json.load(f)
+        except FileNotFoundError:
+            print("Error: File Not Found")
+            return None
 
 
 def main():
-    cleanData("data.json")
+    app = App()
+    app.mainloop()
 
 
 if __name__ == "__main__":
